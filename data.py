71ce081d4b6796f69f52d2fbb40ed8f7687d0960
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-07-18 19:28:57 -0300

    Adds support for cifar10 data and a .gitignore file

--- data.py
+++ data.py
@@ -1,8 +1,8 @@
-from tensorflow.keras.datasets import mnist
+from tensorflow.keras.datasets import mnist, cifar10
 from tensorflow.keras.utils import to_categorical
 import tensorflow as tf
 
-def load_data(batch_size = 32, shuffle_size = 100):
+def load_mnist(batch_size = 32, shuffle_size = 100):
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     
     x_train = x_train / 255
@@ -18,3 +18,18 @@ def load_data(batch_size = 32, shuffle_size = 100):
     test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).shuffle(shuffle_size).batch(batch_size)
     return train_dataset, test_dataset
 
+def load_cifar(batch_size = 32, shuffle_size = 100):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    
+    x_train = x_train / 255
+    x_test = x_test / 255
+
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+    
+    x_train = x_train.reshape(-1, 32*32*3)
+    x_test = x_test.reshape(-1, 32*32*3)
+
+    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train)).shuffle(shuffle_size).batch(batch_size)
+    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).shuffle(shuffle_size).batch(batch_size)
+    return train_dataset, test_dataset
