3b32b00410c55a3f9f3c45f7bb570b211fe765cb
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-07-18 23:08:14 -0300

    Changes train.py to output the loss by using the kerasProgbar

--- train.py
+++ train.py
@@ -4,9 +4,12 @@ import tqdm
 def train_model(model, dataset, epochs):
     for epoch in range(epochs):
         print('doing epoch',epoch)
+        
+        bar = tf.keras.utils.Progbar(tf.data.experimental.cardinality(dataset).numpy())
 
-        for step, (x, y) in enumerate(tqdm.tqdm(dataset)):
-            model.step(x, y)
+        for step, (x, y) in enumerate(dataset):
+            l = model.step(x, y)
+            bar.add(1, values = [('loss', l)])
         
         
         
