27e0c5d42343cc05738629ad21ff4ae6e63df3f9
João Marcos Cardoso da Silva <joaomarcoscsilva@outlook.com>
2020-07-21 11:35:36 -0300

    Adds Weights and Biases logging during training

--- train.py
+++ train.py
@@ -1,11 +1,17 @@
 import tensorflow as tf
 
-def train_model(model, dataset, epochs, test_dataset = None):
+def train_model(model, dataset, epochs, test_dataset = None, wandb_config = None):
     
     # Since for classification the Mixture of Experts loss is the same as categorical_crossentropy, I just call fit here
     # However, there is some code for a custom training loop that must be used if this code is to be adapted for regression
     # This is because the loss for regression in the MoE model is different than the normally used MSE loss.
-    model.fit(dataset, epochs = epochs, validation_data = test_dataset)
+    if wandb_config is not None:
+        import wandb
+        from wandb.keras import WandbCallback
+        wandb.init(project = 'mixture-of-experts', config = wandb_config)
+        model.fit(dataset, epochs = epochs, validation_data = test_dataset, callbacks = [WandbCallback(save_weights_only = True, log_batch_frequency = 128)])
+    else:
+        model.fit(dataset, epochs = epochs, validation_data = test_dataset)
 
     """
     for epoch in range(epochs):
